fb7398a8a2ebbcaefd71a1835fa1b6ec01cc2320
ElenaYKaramysheva <133779037+ElenaYKaramysheva@users.noreply.github.com>
2023-11-08 20:37:00 +0300

    Добавляет селект и попап

--- js/main.js
+++ js/main.js
@@ -41,3 +41,61 @@ const SectionSwiper = new Swiper('.posts-slider', {
       }
     }
   });
+
+/*=============================
+        List-select
+=============================*/
+
+let select = function () {
+  let selectHeader = document.querySelectorAll('.list-select__toggle');
+  let selectOption =  document.querySelectorAll('.list-select__option');
+
+  selectHeader.forEach( (item) => {
+      item.addEventListener('click', selectToggle)
+  }); 
+
+  selectOption.forEach( (item) => {
+      item.addEventListener('click', selectChoose)
+  }); 
+
+  function selectToggle() {
+      this.parentElement.classList.toggle('list-select-active')
+  }
+
+  function selectChoose() {
+      let text = this.innerText;
+      let select = this.closest('.list-select');
+      let currentText = this.closest('.list-select').querySelector('.list-select__toggle');
+      currentText.innerText = text;
+      select.classList.remove('list-select-active');
+  }
+      
+};
+
+select();
+
+/*=============================
+        Popup
+=============================*/
+
+let popup = document.getElementById('form-popup');
+let popupBackground = document.querySelector('.popup-wrapper');
+let popupToggle = document.querySelector('.header__button');
+let popupClose = document.querySelector('.popup__close');
+
+  popupToggle.onclick = function() {
+    popup.style.display="block";
+    popupBackground.style.display="block";
+  };
+
+  popupClose.onclick = function() {
+    popup.style.display="none";
+    popupBackground.style.display="none";
+  };
+
+  window.onclick = function(e) {
+    if(e.target == popupBackground) {
+      popup.style.display="none";
+      popupBackground.style.display="none";
+    }
+  }
